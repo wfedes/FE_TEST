2bf6996fe702776c34c722d1a98cb4b45ba2cc7d
wfedes <wfedes@gmail.com>
2021-09-16 05:13:01 +0900

    feat: header, Title, LNB component import & bg add

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@styles/index.module.scss'
 
+import Header from '@components/header/index'
+import Lnb from '@components/lnb/index'
+
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
@@ -11,7 +14,9 @@ const Home: NextPage = () => {
         <meta name="description" content="FE_TEST page" />
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
-
+      <Header />
+      <h1>Project Name</h1>
+      <Lnb />
       <main className={styles.main}>
         <p>FE_TEST</p>
       </main>
